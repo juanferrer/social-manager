d52a9724ac2330df093caf76a8ad3da970c1ed48
JuanFerrer <juanemilioferrer@outlook.com>
2017-11-29 17:10:35 +0000

    Add login screen layout and images

--- js/index.js
+++ js/index.js
@@ -1,5 +1,16 @@
+/* globals $, feather, Facebook, Twitter */
+
 var facebook = new Facebook();
 var twitter = new Twitter();
+
+feather.replace();
+$("#login-screen").parent().show();
+$("#publish-screen").parent().hide();
+
+function changeScreen() {
+	$("#login-screen").parent().toggle();
+	$("#publish-screen").parent().toggle();
+}
 
 function post(postContent) {
 	facebook.login();
