cf74c2e7d4576dd1fe52c157615c52b2cf5bfd2f
JuanFerrer <juanemilioferrer@outlook.com>
2017-11-30 13:55:26 +0000

    Add screen transitions

--- js/index.js
+++ js/index.js
@@ -4,8 +4,6 @@ var facebook = new Facebook();
 var twitter = new Twitter();
 
 feather.replace();
-$("#login-screen").parent().show();
-$("#publish-screen").parent().hide();
 
 /* Event handlers */
 
@@ -20,8 +18,28 @@ $("#left-button").click(() => {
 /* Functions */
 
 function changeScreen() {
-	$("#login-screen").parent().toggle();
-	$("#publish-screen").parent().toggle();
+	var toLogin = $("#login-screen").parent().css("z-index") === "0";
+	if (toLogin) {
+		$("#publish-screen").css("opacity", "0");
+		$("#publish-screen").css("transform", "translate(-10px)");
+	} else {
+		$("#login-screen").css("opacity", "0");
+		$("#login-screen").css("transform", "translate(10px)");
+	}
+
+	setTimeout((toLogin) => {
+
+		$("#login-screen").parent().css("z-index", toLogin ? 1 : 0);
+		$("#publish-screen").parent().css("z-index", toLogin ? 0 : 1);
+
+		$("#login-screen").css("transform", "translate(0px)");
+		$("#publish-screen").css("transform", "translate(0px)");
+		setTimeout(() => {
+			$("#login-screen").css("opacity", "1");
+			$("#publish-screen").css("opacity", "1");
+		}, 300);
+
+	}, 300, toLogin);
 }
 
 function post(postContent) {
