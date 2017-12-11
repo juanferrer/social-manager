6ee61cb3f0310d47db200832f9871af8eee72a20
JuanFerrer <juanemilioferrer@outlook.com>
2017-12-11 17:27:44 +0000

    WIP: Twitter oauth

--- js/twitter.js
+++ js/twitter.js
@@ -1,4 +1,4 @@
-/* globals $*/
+/* globals $, twitter*/
 
 class Twitter {
 	constructor() {
@@ -12,13 +12,21 @@ class Twitter {
 	login() {
 		$.ajax({
 			url: "https://diabolic-straps.000webhostapp.com/social-manager/twitter.php",
-			data: { action: "login" },
+			data: { action: "login", id: "SOMETHING" },
 			type: "POST"
 		}).done(function (r) {
 			var loginWindow = window.open("", "", "width=550, height=615");
-			loginWindow.
+
+			/*var timer = setInterval(() => {
+				if (loginWindow.location.href.startsWith("https://juanferrer.github.io/social-manager")) {
+					console.log(loginWindow.location.href);
+					//twitter.accessToken = loginWindow.location.href.lastIndexOf();
+					window.close();
+					clearInterval(timer);
+				}
+			}, 500);*/
+
 			loginWindow.location.href = r;
-			//window.location.href = r;
 		});
 	}
 
--- php/twitter.php
+++ php/twitter.php
@@ -8,35 +8,89 @@ require 'twitteroauth/autoload.php';
 
 use Abraham\TwitterOAuth\TwitterOAuth;
 
-// This code will run when returned from twiter after authentication
-if(isset($_SESSION['oauth_token'])){
-  $oauth_token = $_SESSION['oauth_token'];
-  unset($_SESSION['oauth_token']);
-  $consumer_key = '';
-  $consumer_secret = '';
-  $connection = new TwitterOAuth($consumer_key, $consumer_secret);
- //necessary to get access token other wise u will not have permision to get user info
-  $params = array("oauth_verifier" => $_GET['oauth_verifier'], "oauth_token" => $_GET['oauth_token']);
-  $access_token = $connection->oauth("oauth/access_token", $params);
-  //now again create new instance using updated return oauth_token and oauth_token_secret because old one expired if u dont u this u will also get token expired error
-  $connection = new TwitterOAuth($consumer_key, $consumer_secret,
-  $access_token['oauth_token'], $access_token['oauth_token_secret']);
-  $content = $connection->get("account/verify_credentials");
-  header('Location: https://juanferrer.github.io/social-manager?token='.$access_token['oauth_token']);
-}
-else{
-  // Main startup code
-  $consumer_key = '';
-  $consumer_secret = '';
-  // This code will return your valid url which u can use in iframe src to popup or can directly view the page as its happening in this example
+$CONSUMER_KEY = 'omHu0C1KXMXtOW6se9WEIgiA0';
+$CONSUMER_SECRET = 'BCiPqZhVV3LXQzeOn4p4bKmwNJXCJGelW4Cfm7j8x508fcaAFc';
+$OAUTH_CALLBACK = 'https://diabolic-straps.000webhostapp.com/social-manager/twitter.php';
 
-  $connection = new TwitterOAuth($consumer_key, $consumer_secret);
-  $temporary_credentials = $connection->oauth('oauth/request_token', array("oauth_callback" =>'https://diabolic-straps.000webhostapp.com/social-manager/twitter.php'));
-  $_SESSION['oauth_token'] = $temporary_credentials['oauth_token'];
-  $_SESSION['oauth_token_secret'] = $temporary_credentials['oauth_token_secret'];
-  $url = $connection->url("oauth/authorize", array("oauth_token" => $temporary_credentials['oauth_token']));
-// REDIRECTING TO THE URL
-  //print_r($url);
-  header('Location: ' . $url);
+if (!empty($_POST['action'])) {
+    $action = $_POST['action'];
+
+    switch ($action) {
+        case 'login':
+            if (!empty($_POST['id'])) {
+                login($_POST['id']);
+            } else {
+                die('No ID provided');
+            }
+
+            break;
+    }
+} else if (!empty($_GET['oauth_verifier'])) {
+    $link = mysqli_connect('localhost', 'id2777537_social_manager', 'm794ja!2Y5C!', 'id2777537_twitter_logins');
+    if (!$link) {
+        die('Server error');
+    }
+
+    $oauth = [];
+
+    // Check if the oauth is populated
+    if ($oauth_result = mysqli_query($link, 'SELECT * FROM oauth_tokens WHERE oauth_token = ' . $_GET['oauth_token'])) {
+        $oauth = mysqli_fetch_array($oauth_result);
+    }
+
+    mysqli_query($link,
+        'UPDATE oauth_tokens SET oauth_verifier=' . $_GET['oauth_verifier'] . ' WHERE oauth_token=' . $_GET['oauth_token']);
+
+    login($oauth['id']);
 }
-?>
+
+function login($id)
+{
+    global $CONSUMER_KEY, $CONSUMER_SECRET, $OAUTH_CALLBACK;
+
+    $link = mysqli_connect('localhost', 'id2777537_social_manager', 'm794ja!2Y5C!', 'id2777537_twitter_logins');
+    if (!$link) {
+        die('Server error');
+    }
+
+    $oauth = [];
+
+    // Check if the oauth is populated
+    if ($oauth_result = mysqli_query($link, 'SELECT * FROM oauth_tokens WHERE id = ' . $id)) {
+        $oauth = mysqli_fetch_array($oauth_result);
+    }
+
+    // If no oauth_token yet, generate a new one and store
+    if (empty($oauth['oauth_token'])) {
+        $connection = new TwitterOAuth($CONSUMER_KEY, $CONSUMER_SECRET);
+        $temporary_credentials = $connection->oauth('oauth/request_token', array('oauth_callback' => $OAUTH_CALLBACK));
+        mysqli_query($link,
+            'INSERT INTO oauth_tokens (id, oauth_token, oauth_token_secret, oauth_verifier) VALUES(' .
+            $id . ', ' .
+            $temporary_credentials['oauth_token'] . ', ' .
+            $temporary_credentials['oauth_token_secret'] . ')');
+
+        $url = $connection->url('oauth/authorize', array('oauth_token' => $temporary_credentials['oauth_token']));
+
+        print_r($url);
+    } else {
+        $connection = new TwitterOAuth($CONSUMER_KEY, $CONSUMER_SECRET);
+
+        // Get access token
+        $params = array('oauth_verifier' => $oauth['oauth_verifier'], 'oauth_token' => $oauth['oauth_token']);
+        $access_token = $connection->oauth('oauth/access_token', $params);
+
+        // Update tokens
+        $connection = new TwitterOAuth($CONSUMER_KEY, $CONSUMER_SECRET,
+            $access_token['oauth_token'], $access_token['oauth_token_secret']);
+        $content = $connection->get('account/verify_credentials');
+
+        mysqli_query($link,
+            'UPDATE oauth_tokens SET' .
+            'oauth_token=' . $access_token['oauth_token'] . ', ' .
+            'oauth_token_secret=' . $access_token['oauth_token_secret'] . ', ' .
+            ' WHERE id=' . $id);
+
+        header('Location: https://juanferrer.github,io/social-manager?token=' . $access_token['oauth_access_token']);
+    }
+}
