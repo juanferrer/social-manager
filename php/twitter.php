0b50cad469ea1b9a760fbb7c6b2594f44f6cbe7a
JuanFerrer <juanemilioferrer@outlook.com>
2017-12-12 10:43:35 +0000

    Add Twitter login

--- php/twitter.php
+++ php/twitter.php
@@ -24,6 +24,10 @@ if (!empty($_POST['action'])) {
             }
 
             break;
+
+        case 'post':
+            post($_POST['id'], $_POST['message']);
+            break;
     }
 } else if (!empty($_GET['oauth_verifier'])) {
     $link = mysqli_connect('localhost', 'id2777537_social_manager', 'm794ja!2Y5C!', 'id2777537_twitter_logins');
@@ -34,15 +38,14 @@ if (!empty($_POST['action'])) {
     $oauth = [];
 
     // Check if the oauth is populated
-    if ($oauth_result = mysqli_query($link, 'SELECT * FROM oauth_tokens WHERE oauth_token = ' . $_GET['oauth_token'])) {
+    if ($oauth_result = mysqli_query($link, 'SELECT * FROM `oauth_tokens` WHERE `oauth_token` = "' . $_GET['oauth_token'] . '"')) {
         $oauth = mysqli_fetch_array($oauth_result);
     }
 
     mysqli_query($link,
-        'UPDATE oauth_tokens SET oauth_verifier=' . $_GET['oauth_verifier'] . ' WHERE oauth_token=' . $_GET['oauth_token']);
+        'UPDATE `oauth_tokens` SET `oauth_verifier`="' . $_GET['oauth_verifier'] . '" WHERE `oauth_token`="' . $_GET['oauth_token'] . '"');
 
-    //ogin($oauth['id']);
-    print_r($_GET['oauth_token']);
+    login($oauth['id']);
 }
 
 function login($id)
@@ -54,27 +57,22 @@ function login($id)
     $oauth = [];
 
     // Check if the oauth is populated
-    if ($oauth_result = mysqli_query($link, 'SELECT * FROM oauth_tokens WHERE id = ' . $id)) {
+    if ($oauth_result = mysqli_query($link, 'SELECT * FROM `oauth_tokens` WHERE `id` = "' . $id . '"')) {
         $oauth = mysqli_fetch_array($oauth_result);
     }
-
     // If no oauth_token yet, generate a new one and store
     if (empty($oauth['oauth_token'])) {
         $connection = new TwitterOAuth($CONSUMER_KEY, $CONSUMER_SECRET);
         $temporary_credentials = $connection->oauth('oauth/request_token', array('oauth_callback' => $OAUTH_CALLBACK));
-        $sql = 'INSERT INTO oauth_tokens (id, oauth_token, oauth_token_secret) VALUES(' .
-            $id . ', ' .
-            $temporary_credentials['oauth_token'] . ', ' .
-            $temporary_credentials['oauth_token_secret'] . ')';
-        if (!mysqli_query($link, $sql)) {
-            echo ("Error " . mysqli_connect_errno());
-        }
+        $sql = 'INSERT INTO `oauth_tokens` (`id`, `oauth_token`, `oauth_token_secret`) VALUES ("' .
+            $id . '", "' .
+            $temporary_credentials['oauth_token'] . '", "' .
+            $temporary_credentials['oauth_token_secret'] . '")';
         $url = $connection->url('oauth/authorize', array('oauth_token' => $temporary_credentials['oauth_token']));
-
+        mysqli_query($link, $sql);
         print_r($url);
     } else {
         $connection = new TwitterOAuth($CONSUMER_KEY, $CONSUMER_SECRET);
-
         // Get access token
         $params = array('oauth_verifier' => $oauth['oauth_verifier'], 'oauth_token' => $oauth['oauth_token']);
         $access_token = $connection->oauth('oauth/access_token', $params);
@@ -85,11 +83,16 @@ function login($id)
         $content = $connection->get('account/verify_credentials');
 
         mysqli_query($link,
-            'UPDATE oauth_tokens SET' .
-            'oauth_token=' . $access_token['oauth_token'] . ', ' .
-            'oauth_token_secret=' . $access_token['oauth_token_secret'] . ', ' .
-            ' WHERE id=' . $id);
-
-        header('Location: https://juanferrer.github,io/social-manager?token=' . $access_token['oauth_access_token']);
+            'UPDATE `oauth_tokens` SET' .
+            '`oauth_token`="' . $access_token['oauth_token'] . '", ' .
+            '`oauth_token_secret`="' . $access_token['oauth_token_secret'] .
+            '" WHERE `id`="' . $id . '"');
+        print_r($access_token);
+        header('Location: https://juanferrer.github,io/social-manager?token=' . $access_token['access_token']);
     }
+}
+
+function post($id, $message)
+{
+
 }
